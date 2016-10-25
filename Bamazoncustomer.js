db0402cac18058a1aa7f932af45afb8cd20bcb95
Vikas Sundrani <sundrani.vikas@gmail.com>
2016-10-24 23:10:49 -0400

    db calling

---
+++ Bamazoncustomer.js
@@ -0,0 +1,45 @@
+var mysql = require ('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "Mushu86212!",
+	database:"bamazon"
+})
+
+connection.connect(function(err){
+	if (err) throw err;
+	runApp();
+})
+
+var runApp = function(){
+	displayProducts();
+// 	inquirer.prompt({
+// 		name: "action",
+// 		message: "Which product would you like to buy?"
+// 	})
+}
+
+var displayProducts = function(){
+	connection.query('SELECT * FROM products', function(err, res){
+		if (err) throw err;
+		console.log(res);
+		inquirer.prompt({
+			name: "choice",
+			type: "rawlist",
+			choices: function(value){
+				var choiceArray = [];
+				for (var i = 0; i < res.length; i++){
+					choiceArray.push(res[i].ProductName);
+				}
+				return choiceArray;
+			},
+			message: "Which item would you like to buy?"
+		}).then(function(answer){
+			
+		}
+	})
+
+}
